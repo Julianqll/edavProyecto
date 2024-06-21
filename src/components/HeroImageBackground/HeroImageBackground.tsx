44ec3ff37e9a9dd80f3c52d8cf2e5879aef9dda6
Julian Quispe <julianmql@hotmail.com>
2024-06-21 17:16:15 -0500

    feat: home file trigger added

--- src/components/HeroImageBackground/HeroImageBackground.tsx
+++ src/components/HeroImageBackground/HeroImageBackground.tsx
@@ -1,21 +1,29 @@
 import { Title, Flex, Box } from '@mantine/core';
 import classes from './HeroImageBackground.module.css';
 import { FileOption } from '../FileOption/FileOption';
-import { open } from '@tauri-apps/api/dialog';
-
-async function handleDialog(): Promise<void> {
-  const selected = await open({
-    multiple: true,
-    filters: [{
-      name: 'Image',
-      extensions: ['bin']
-    }]
-  });
-  console.log(selected);
-}
-
+import { open as tauriOpen } from '@tauri-apps/api/dialog';
+import { useNavigate } from 'react-router-dom';
 
 export function HeroImageBackground() {
+  const navigate = useNavigate();
+
+  const handleDialog = (type: string) => {
+    return async (): Promise<void> => {
+      const selected = await tauriOpen({
+        multiple: true,
+        filters: [{
+          name: 'Image',
+          extensions: [type == 'binary' ? '.bin' : '.txt']
+        }]
+      });
+      if (selected && selected.length > 0) {
+        const selectedFile = selected[0];
+        const fileType = type; 
+        navigate('/menu', { state: { selectedFile, fileType } });
+      }
+    };
+  };
+
   return (
     <Box 
       p={50} 
@@ -23,30 +31,30 @@ export function HeroImageBackground() {
       style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
     >
       <Flex
-      mih={50}
-      gap="md"
-      justify="center"
-      align="center"
-      direction="column"
-      wrap="wrap"
-    >
-      <Title mt={20} mb={50} className={classes.title}>
-        Proyecto final de EDAV
-      </Title>
-      <Flex
-        mb={20}
         mih={50}
-        gap={90}
+        gap="md"
         justify="center"
         align="center"
-        direction="row"
-        wrap={{ base: 'wrap', sm: 'nowrap'}}
-        style={{ width: '100%' }}
+        direction="column"
+        wrap="wrap"
+      >
+        <Title mt={20} mb={50} className={classes.title}>
+          Proyecto final de EDAV
+        </Title>
+        <Flex
+          mb={20}
+          mih={50}
+          gap={90}
+          justify="center"
+          align="center"
+          direction="row"
+          wrap={{ base: 'wrap', sm: 'nowrap'}}
+          style={{ width: '100%' }}
         >
-          <FileOption type="text" handler={handleDialog} />
-          <FileOption type="binary" handler={handleDialog} />
+          <FileOption type="text" handler={handleDialog('txt')} />
+          <FileOption type="binary" handler={handleDialog('bin')} />
+        </Flex>
       </Flex>
-    </Flex>
     </Box>
   );
 }
